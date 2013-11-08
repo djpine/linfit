a4951736eb52f41748b3755512a7b67c9d5542a2
David Pine <pine@nyu.edu>
2013-11-08 12:22:43 -0500

    Create test_linfit.py
    Unit tests for linfit.py

---
+++ test_linfit.py
@@ -0,0 +1,240 @@
+""" Unit tests for linfit
+Author: David Pine
+April 2013
+"""
+from linfit import linfit
+from numpy import polyfit
+import numpy as np
+from scipy.linalg import lstsq
+import time
+import unittest
+
+EPS1 = 1.0e-13
+EPS2 = 1.0e-10
+
+def randomData(xmax, npts):
+    x = np.random.uniform(-xmax, xmax, npts)
+    scale = np.sqrt(xmax)
+    a, b = scale * (np.random.rand(2)-0.5)
+    y = a*x + b + a * scale * np.random.randn(npts)
+    dy = a * scale * (1.0 + np.random.rand(npts))
+    return x, y, dy
+
+#-------------------------------------------------------------------------------
+# Tests
+#-------------------------------------------------------------------------------
+
+class Testlinfitear_BadInputs(unittest.TestCase):
+
+    def test_arraySizeTooSmall(self):
+        # check that TypeError is raised for input arrays with length 2
+        x = np.array([0, 1])
+        y = np.array([2, 3])
+        self.assertRaises(TypeError, linfit, x, y)
+
+    def test_arraySizeUnequalxy(self):
+        # check that TypeError is raised if input arrays have unequal length
+        x = np.array([0, 1, 2, 3])
+        y = np.array([2, 3, 4])
+        self.assertRaises(TypeError, linfit, x, y)
+
+    def test_arraySizeUnequalydy(self):
+        # check that TypeError is raised if y & dy arrays have unequal length
+        x = np.array([0, 1, 2, 3])
+        y = np.array([2, 3, 4, 5])
+        dy = np.array([2, 3, 4])
+        self.assertRaises(TypeError, linfit, x, y, sigmay=dy)
+
+class Testlinfit_PerfectFits(unittest.TestCase):
+    
+    def test_pfits(self):
+        # check that perfect straight lines are fit perfectly
+        x = np.random.uniform(-100.0, 100.0, 10)
+        a, b = np.random.rand(2)
+        y = a*x+b
+        fit, cvm, redchisq, residuals = linfit(x, y, cov=True, residuals=True)
+        dfit = [np.sqrt(cvm[i,i]) for i in range(2)]
+        diffa, diffb = np.abs(a-fit[0]), np.abs(b-fit[1])
+        self.assertTrue(diffa<EPS1 and diffb<EPS1)
+        self.assertTrue(np.abs(dfit[0])<EPS1 and np.abs(dfit[1])<EPS1)
+        self.assertTrue(np.abs(cvm[0,1])<EPS1*EPS1)
+        self.assertTrue(redchisq<EPS1*EPS1)
+
+class Testlinfit_CompareWithOtherFits(unittest.TestCase):
+    # Check that linfit gives the same results as other scipy fitting routines
+
+    def test_polyfitWtInd(self):
+        # check that get same answers as NumPy polyfit when relative weighting
+        # (only type of weighting currently available in polyfit) is used.
+        # Also compare run times.
+        npts = 100
+        x, y, dy = randomData(100., npts)
+        wts = 1./dy
+        fit, cvm = linfit(x, y, sigmay=dy, relsigma=True, cov=True)
+        pfit, v = polyfit(x, y, 1, w=wts, cov=True)
+        diff_fit = np.abs((pfit - fit)/fit).sum()/2.0
+        # polyfit uses a nonstandard normalization of the covariance
+        # matrix.  It is corrected here for comparison with linfit, which
+        # uses the standard definition.
+        v_corrected = v * (npts-4.)/(npts-2.)
+        diff_cvm = np.abs((v_corrected - cvm)/cvm).sum()/4.0
+        self.assertTrue(diff_fit<EPS2)
+        self.assertTrue(diff_cvm<EPS2)
+
+    def test_polyfitWtSame(self):
+        # check that get same answers as NumPy polyfit when relative weighting
+        # (only type of weighting currently available in polyfit) is used.
+        npts = 100
+        x, y, dy = randomData(100., npts)
+        dy = np.random.rand(1)
+        wts = np.ones(y.size)/dy
+        fit, cvm = linfit(x, y, sigmay=dy, relsigma=True, cov=True)
+        pfit, v = polyfit(x, y, 1, w=wts, cov=True)
+        diff_fit = np.abs((pfit - fit)/fit).sum()/2.0
+        # polyfit uses a nonstandard normalization of the covariance
+        # matrix.  It is corrected here for comparison with linfit, which
+        # uses the standard definition.
+        v_corrected = v * (npts-4.)/(npts-2.)
+        diff_cvm = np.abs((v_corrected - cvm)/cvm).sum()/4.0
+        self.assertTrue(diff_fit<EPS2)
+        self.assertTrue(diff_cvm<EPS2)
+
+    def test_linalg_lstsqNoWt(self):
+        # check that get same answers as Scipy linalg_lstsq when all weighting
+        # is turned off.
+        npts = 100
+        x, y, dy = randomData(100., npts)
+        fit = linfit(x, y)
+        X = np.vstack([np.ones(npts), x]).T
+        cNoWts, resid, rank, sigma = lstsq(X, y)
+        diff_fit = np.abs((np.flipud(cNoWts)-fit)/fit).sum()/2.0
+        self.assertTrue(diff_fit<EPS2)
+
+    def test_linalg_lstsqWt(self):
+        # check that get same answers as Scipy linalg_lstsq using weighting
+        npts = 100
+        x, y, dy = randomData(100., npts)
+        fit = linfit(x, y, sigmay=dy)
+        X = np.vstack([np.ones(npts), x]).T
+        A = X/np.array(zip(dy,dy))
+        cWts, resid, rank, sigma = lstsq(A, y/dy)
+        diff_fit = np.abs((np.flipud(cWts)-fit)/fit).sum()/2.0
+        self.assertTrue(diff_fit<EPS2)
+
+class Testlinfit_CompareWithOtherFitsRunTime(unittest.TestCase):
+    # These tests compare the times for linfit to fit data sets to other
+    # functions in scipy
+
+    def test_polyfitWtIndTIME(self):
+        # Compare runtime of linfit vs polyfit when relative weighting
+        # (only type of weighting currently available in polyfit) is used.
+        nruns = 10
+        print('\nCompare linfit to polyfit with individually weighted data points')
+        for npts in [10, 100, 1000, 10000, 100000, 1000000]:
+            x, y, dy = randomData(100., npts)
+            wts = 1./dy
+            linfitTIMEmin = 100000.
+            linfitTIMEsum = 0.
+            for i in range(nruns):
+                start = time.time()
+                fit, cvm = linfit(x, y, sigmay=dy, relsigma=True, cov=True)
+                fittime = (time.time() - start)
+                linfitTIMEsum += fittime
+                if fittime<linfitTIMEmin: linfitTIMEmin=fittime
+            polyfitTIMEmin = 100000.
+            polyfitTIMEsum = 0.
+            for i in range(nruns):
+                start = time.clock()
+                pfit, v = polyfit(x, y, 1, w=wts, cov=True)
+                fittime = (time.clock() - start)
+                polyfitTIMEsum += fittime
+                if fittime<polyfitTIMEmin: polyfitTIMEmin=fittime
+            print("{0:7d} data points: linfit is faster than polyfit by {1:0.2g} ({2:0.2g}) times"
+                  .format(npts, polyfitTIMEmin/linfitTIMEmin, polyfitTIMEsum/linfitTIMEsum))
+
+    def test_polyfitWtSameTIME(self):
+        # Compare runtime of linfit vs polyfit when relative weighting
+        # (only type of weighting currently available in polyfit) is used.
+        nruns = 10
+        print('\nCompare linfit to polyfit with equally weighted data points')
+        for npts in [10, 100, 1000, 10000, 100000, 1000000]:
+            x, y, dy = randomData(100., npts)
+            dy = np.random.rand(1)
+            wts = np.ones(y.size)/dy
+            linfitTIMEmin = 100000.
+            linfitTIMEsum = 0.
+            for i in range(nruns):
+                start = time.clock()
+                fit, cvm = linfit(x, y, sigmay=dy, relsigma=True, cov=True)
+                fittime = (time.clock() - start)
+                linfitTIMEsum += fittime
+                if fittime<linfitTIMEmin: linfitTIMEmin=fittime
+            polyfitTIMEmin = 100000.
+            polyfitTIMEsum = 0.
+            for i in range(nruns):
+                start = time.clock()
+                pfit, v = polyfit(x, y, 1, w=wts, cov=True)
+                fittime = (time.clock() - start)
+                polyfitTIMEsum += fittime
+                if fittime<polyfitTIMEmin: polyfitTIMEmin=fittime
+            print("{0:7d} data points: linfit is faster than polyfit by {1:0.2g} ({2:0.2g}) times"
+                  .format(npts, polyfitTIMEmin/linfitTIMEmin, polyfitTIMEsum/linfitTIMEsum))
+
+    def test_linalg_lstsqNoWtTIME(self):
+        # Compare runtimes of linfit vs scipy.linalg_lstsq when all weighting
+        # is turned off.
+        nruns = 10
+        print('\nCompare linfit to linalg.lstsq without weighting data')
+        for npts in [10, 100, 1000]:
+            x, y, dy = randomData(100., npts)
+            linfitTIMEmin = 100000.
+            linfitTIMEsum = 0.
+            for i in range(nruns):
+                start = time.clock()
+                fit = linfit(x, y)
+                fittime = (time.clock() - start)
+                linfitTIMEsum += fittime
+                if fittime<linfitTIMEmin: linfitTIMEmin=fittime
+            linalgfitTIMEmin = 100000.
+            linalgfitTIMEsum = 0.
+            for i in range(nruns):
+                start = time.clock()
+                X = np.vstack([np.ones(npts), x]).T
+                cNoWts, resid, rank, sigma = lstsq(X, y)
+                fittime = (time.clock() - start)
+                linalgfitTIMEsum += fittime
+                if fittime<linalgfitTIMEmin: linalgfitTIMEmin=fittime
+            print("{0:7d} data points: linfit is faster than linalg.lstsq by {1:0.4g} ({2:0.4g}) times"
+                  .format(npts, linalgfitTIMEsum/linfitTIMEmin, linalgfitTIMEsum/linfitTIMEsum))
+
+    def test_linalg_lstsqWtTIME(self):
+        # Compare runtimes of linfit vs scipy.linalg_lstsq using weighting
+        nruns = 10
+        print('\nCompare linfit to linalg.lstsq with weighting data')
+        for npts in [10, 100, 1000]:
+            x, y, dy = randomData(100., npts)
+            linfitTIMEmin = 100000.
+            linfitTIMEsum = 0.
+            for i in range(nruns):
+                start = time.clock()
+                fit = linfit(x, y, sigmay=dy)
+                fittime = (time.clock() - start)
+                linfitTIMEsum += fittime
+                if fittime<linfitTIMEmin: linfitTIMEmin=fittime
+            linalgfitTIMEmin = 100000.
+            linalgfitTIMEsum = 0.
+            for i in range(nruns):
+                start = time.clock()
+                X = np.vstack([np.ones(npts), x]).T
+                A = X/np.array(zip(dy,dy))
+                cWts, resid, rank, sigma = lstsq(A, y/dy)
+                fittime = (time.clock() - start)
+                linalgfitTIMEsum += fittime
+                if fittime<linalgfitTIMEmin: linalgfitTIMEmin=fittime
+            print("{0:7d} data points: linfit is faster than linalg.lstsq by {1:0.4g} ({2:0.4g}) times"
+                  .format(npts, linalgfitTIMEsum/linfitTIMEmin, linalgfitTIMEsum/linfitTIMEsum))
+
+            
+if __name__ == '__main__':
+    import sys
+    unittest.main(testRunner=unittest.TextTestRunner(stream=sys.stdout))
