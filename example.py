366616c29bd7473e85d36d7d8cc15fd99b51f5cb
djpine <pine@nyu.edu>
2014-09-03 15:43:50 -0400

    updated input/ouput of linfit for bunch dictionary

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+from __future__ import division, print_function, absolute_import
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec  # for unequal plot boxes
@@ -9,38 +10,37 @@ y = np.array([-25., 3., 110., 110., 230., 300., 270., 320., 450., 400.])
 sigmay = np.array([15., 30., 30., 40., 40., 50., 40., 30., 50., 30.])
 
 # Fit linear data set with weighting
-fit, cvm, redchisq, residuals = linfit(x, y, sigmay, cov=True, relsigma=False, 
-                                       chisq=True, residuals=True)
-dfit = [np.sqrt(cvm[i,i]) for i in range(2)]
+fit, cvm, info = linfit(x, y, sigmay, relsigma=False, return_all=True)
+dfit = np.sqrt(np.diag(cvm))    # uncertainty estimates for fitting parameters
 
 # Open figure window for plotting data with linear fit
-fig1 = plt.figure(1, figsize=(8,8))
+fig = plt.figure(1, figsize=(8,8))
 gs = gridspec.GridSpec(2, 1, height_ratios=[2.5, 6])
 
 # Bottom plot: data and fit
-ax1 = fig1.add_subplot(gs[1])
+ax1 = fig.add_subplot(gs[1])
 
-# Plot data with error bars on top of fit
+# Plot data with error bars
 ax1.errorbar(x, y, yerr = sigmay, ecolor="black", fmt="ro", ms=5)
 
 # Plot fit (behind data)
-endt = 0.05 * (x.max()-x.min())
-tFit = np.array([x.min()-endt, x.max()+endt])
-vFit = fit[0]*tFit + fit[1]
-ax1.plot(tFit, vFit, "-b", zorder=-1)
+endx = 0.05 * (x.max()-x.min())
+xFit = np.array([x.min()-endx, x.max()+endx])
+yFit = fit[0]*xFit + fit[1]
+ax1.plot(xFit, yFit, "-b", zorder=-1)
 
 # Print out results of fit on plot
-ax1.text(0.05, 0.9,
+ax1.text(0.05, 0.9,     # slope of fit
     u"slope = {0:0.1f} \xb1 {1:0.1f}".format(fit[0], dfit[0]),
     ha="left", va="center", transform = ax1.transAxes)
-ax1.text(0.05, 0.83,
+ax1.text(0.05, 0.83,    # y-intercept of fit
     u"y-intercept = {0:0.1f} \xb1 {1:0.1f}".format(fit[1], dfit[1]),
     ha="left", va="center", transform = ax1.transAxes)
-ax1.text(0.05, 0.76,
-    "redchisq = {0:0.2f}".format(redchisq),
+ax1.text(0.05, 0.76,    # reduced chi-squared of fit
+    "redchisq = {0:0.2f}".format(info.rchisq),
     ha="left", va="center", transform = ax1.transAxes)
-ax1.text(0.05, 0.69,
-    "r = {0:0.2f}".format(cvm[0,1]/(dfit[0]*dfit[1])),
+ax1.text(0.05, 0.69,    # correlation coefficient of fitted slope & y-intercept
+    "rcov = {0:0.2f}".format(cvm[0,1]/(dfit[0]*dfit[1])),
     ha="left", va="center", transform = ax1.transAxes)
 
 # Label axes
@@ -48,9 +48,9 @@ ax1.set_xlabel("time")
 ax1.set_ylabel("velocity")
 
 # Top plot: residuals
-ax2 = fig1.add_subplot(gs[0])
+ax2 = fig.add_subplot(gs[0])
 ax2.axhline(color="gray", zorder = -1)
-ax2.errorbar(x, residuals, yerr = sigmay, ecolor="black", fmt="ro", ms=5)
+ax2.errorbar(x, info.resids, yerr = sigmay, ecolor="black", fmt="ro", ms=5)
 ax2.set_ylabel("residuals")
 ax2.set_ylim(-100, 150)
 ax2.set_yticks((-100, 0, 100))
