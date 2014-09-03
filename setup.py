a9ea7a8e1a8da00e6382cc944599714fad5d2a06
David Pine <pine@nyu.edu>
2014-09-03 16:55:36 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+# Licensed under The Lesser GNU Public License Version 2 (or later)
+
+from setuptools import setup, find_packages
+
+setup(
+    author="David Pine",
+    description="Least squares linear fit for numpy library of Python",
+    license="LGPL2",
+    name="linfit",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    url="https://github.com/djpine/linfit",
+    download_url="https://github.com/djpine/linfit",
+    version="2014.09.03",
+)
